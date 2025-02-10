aa922f5a3fddf0191e130ab2b05dba1379b7612a
Soumyadeep Das <soumyadeep.dbsslg@gmail.com>
2025-02-10 19:31:26 +0530

    initial commit

---
+++ personal/src/main/java/com/example/personal/CoderevProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderevProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderevProjectApplication.class, args);
+	}
+
+}
