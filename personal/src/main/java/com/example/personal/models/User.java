f4a747574fb7d4a617f3ed3bc09a17edd84b0fa1
Soumyadeep Das <soumyadeep.dbsslg@gmail.com>
2025-03-27 13:27:44 +0530

    removed redundant field

--- personal/src/main/java/com/example/personal/models/User.java
+++ personal/src/main/java/com/example/personal/models/User.java
@@ -15,7 +15,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = false, nullable = false)
+    @Column(nullable = false)
     private String username;
 
     @Column(unique = true, nullable = false)
