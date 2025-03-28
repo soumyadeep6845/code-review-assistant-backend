fce110fc43f6de5f39c1401d45d52ccd86437ba4
Soumyadeep Das <soumyadeep.dbsslg@gmail.com>
2025-03-28 18:43:25 +0530

    updated security fix

--- personal/src/main/java/com/example/personal/security/JwtUtil.java
+++ personal/src/main/java/com/example/personal/security/JwtUtil.java
@@ -3,6 +3,8 @@ package com.example.personal.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import java.util.Date;
@@ -15,13 +17,15 @@ public class JwtUtil {
     private String SECRET_KEY;
     private static final long EXPIRATION_TIME = 86400000; // 24 hours
     private static final Set<String> activeTokens = new HashSet<>();
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtil.class);
 
     public String generateToken(String email) {
         String token = JWT.create()
                 .withSubject(email)
                 .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .sign(Algorithm.HMAC256(SECRET_KEY));
-        activeTokens.add(token); // Store token in a whitelist
+        activeTokens.add(token); // Adding token in active sessions list
+        logger.info("Stored in active sessions list. Token: " + activeTokens.toString());
         return token;
     }
 
@@ -36,7 +40,12 @@ public class JwtUtil {
                     .build()
                     .verify(token)
                     .getSubject();
-            return securityToken; // Token must be in active session list
+
+            if(activeTokens.toString().contains(token)){ // Token must be present in active sessions list
+                return securityToken;
+            } else{
+                return null;
+            }
         } catch (JWTVerificationException e) {
             return null;
         }
